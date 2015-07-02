bec56c0dd608ea71be6018a093a0b7fa932abcef
高考这件尛事 <pupboss.i@gmail.com>
2015-07-02 17:53:19 +0800

    Update CaptureConfig.java

--- src/main/java/com/lntu/online/server/capture/CaptureConfig.java
+++ src/main/java/com/lntu/online/server/capture/CaptureConfig.java
@@ -22,8 +22,8 @@ package com.lntu.online.server.capture;
 public final class CaptureConfig {
 
     private static final String[] SERVER_RULS = {
-            "http://60.18.131.131:11180/academic",
             "http://60.18.131.131:11080/newacademic",
+            "http://60.18.131.131:11180/academic",
             "http://60.18.131.133:11180/newacademic",
             "http://60.18.131.133:11181/newacademic"
     };
