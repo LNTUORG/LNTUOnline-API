e7d5e6e4b6f0484a799945f1cfa255deb0821f2f
TakWolf <takwolf@foxmail.com>
2015-09-25 23:00:54 +0800

    崩溃日志结构

---
+++ src/main/java/com/lntu/online/server/resource/FeedbackResource.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015-2016 LNTU.ORG (http://lntu.org)
+ * Copyright (C) 2014-2015 TakWolf <takwolf@foxmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.lntu.online.server.resource;
+
+import com.lntu.online.server.dao.CrashLog;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+import java.util.UUID;
+
+@Path("feedback")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class FeedbackResource {
+
+    @POST
+    @Path("crash-log")
+    public void crashLog(
+            @HeaderParam("User-Agent") String userAgent,
+            @FormParam("userId") String userId,
+            @FormParam("content") String content
+    ) {
+        CrashLog crashLog = new CrashLog();
+        crashLog.setId(UUID.randomUUID().toString());
+        crashLog.setUserId(userId);
+        crashLog.setUserAgent(userAgent);
+        crashLog.setContent(content);
+        crashLog.setCreateAt(new Date());
+        crashLog.save();
+    }
+
+}
